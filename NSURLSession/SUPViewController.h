cc20df4949aa7ba646d14c05232dcfa6c71b4c22
Oisin Prendiville <poppoppop@gmail.com>
2013-09-08 17:39:05 +0100

    Set up and resume tasks and log callbacks from delegate and completion handler

--- NSURLSession/SUPViewController.h
+++ NSURLSession/SUPViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SUPViewController : UIViewController
+@interface SUPViewController : UIViewController <NSURLSessionDownloadDelegate>
 
 @end
--- NSURLSession/SUPViewController.m
+++ NSURLSession/SUPViewController.m
@@ -10,6 +10,11 @@
 
 @interface SUPViewController ()
 
+@property (nonatomic) NSURLSession *session;
+@property (nonatomic) NSOperationQueue *operationQueue;
+@property (nonatomic) NSURLSessionDownloadTask *taskWithCompletionHandler;
+@property (nonatomic) NSURLSessionDownloadTask *taskWithoutCompletionHandler;
+
 @end
 
 @implementation SUPViewController
@@ -18,6 +23,21 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.operationQueue = [[NSOperationQueue alloc]init];
+    self.session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:self delegateQueue:self.operationQueue];
+    
+    NSURLRequest *request = [[NSURLRequest alloc]initWithURL:[NSURL URLWithString:@"http://resources.usetokens.com/deployed_assets/07092013213943/images/Tokens.iconset/icon_512x512@2x.png"]];
+    
+    // When I resume this task the delegate methods are never called because it has a completion handler
+    self.taskWithoutCompletionHandler = [self.session downloadTaskWithRequest:request completionHandler:^(NSURL *location, NSURLResponse *response, NSError *error) {
+        NSLog(@"completionHandler");
+    }];
+    
+    // Delegate methods are called as expected on this task without a completion handler
+    self.taskWithoutCompletionHandler = [self.session downloadTaskWithRequest:request completionHandler:nil];
+    
+    [self.taskWithCompletionHandler resume];
+    [self.taskWithoutCompletionHandler resume];
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +45,32 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - NSURLSessionDownloadDelegate
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask
+didFinishDownloadingToURL:(NSURL *)location
+{
+    NSLog(@"didFinishDownloadingToURL %@",location);
+}
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask
+      didWriteData:(int64_t)bytesWritten
+ totalBytesWritten:(int64_t)totalBytesWritten
+totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite
+{
+    if (downloadTask == self.taskWithCompletionHandler)
+    {
+        NSLog(@"didWriteData for task with comletion handler");
+    }
+    else if (downloadTask == self.taskWithoutCompletionHandler)
+    {
+        NSLog(@"didWriteDate for task without completion handler");
+    }
+}
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask
+ didResumeAtOffset:(int64_t)fileOffset
+expectedTotalBytes:(int64_t)expectedTotalBytes{}
 
 @end
